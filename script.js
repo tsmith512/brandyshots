d6a962b246beea91b00df742238b4673fc26b58a
Taylor Smith <tsmith@tsmithcreative.com>
2019-05-16 19:54:56 +0000

    Add a window size and a timeout to allow async stuff to load

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var page = require('webpage').create();
+page.viewportSize = {width: 1920, height: 1080};
 page.open('http://github.com/', function() {
-  page.render('github.png');
-  phantom.exit();
+  window.setTimeout(function(){
+    page.render('github.png');
+    phantom.exit();
+  }, 500);
 });
