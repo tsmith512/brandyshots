995e819a7b4c7500c34bea93c11505bd10cb455e
Taylor Smith <tsmith@tsmithcreative.com>
2019-05-16 23:55:14 +0000

    Add new file using node-crawler to actually find and pull URLs in the site

---
+++ crawl.js
@@ -0,0 +1,41 @@
+const Crawler = require("crawler");
+// const puppeteer = require('puppeteer');
+
+let alreadySeen = [];
+
+const c = new Crawler();
+
+// Adapted from https://stackoverflow.com/a/50164565
+
+const crawlAll = function(parentUrl) {
+  console.log("Crawling " + parentUrl);
+
+  c.queue({
+    url: parentUrl,
+    callback: function (err, res, done) {
+      if (err) throw err;
+
+      const $ = res.$;
+
+      try {
+        const childLinks = $("a");
+        Object.keys(childLinks).forEach((index) => {
+          if (childLinks[index].type === 'tag') {
+            const href = childLinks[index].attribs.href.trim();
+
+            if (href && !alreadySeen.includes(href)) {
+              alreadySeen.push(href);
+              crawlAll(href);
+            }
+          }
+        });
+      } catch (e) {
+        console.error("Encountered error crawling" + parentUrl);
+        done();
+      }
+      done();
+      }
+  })
+}
+
+crawlAll('https://www.tsmithcreative.com');
