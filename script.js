d986a7b0000c970e7ba2dffe496bf13e6aa92c13
Taylor Smith <tsmith@tsmithcreative.com>
2019-05-17 19:52:59 +0000

    Test taking screenshots from an array

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const puppeteer = require('puppeteer');
 
+let shotList = ['https://www.github.com', 'http://www.example.com', 'https://fourkitchens.com'];
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.tsmithcreative.com');
   await page.setViewport({width: 1920, height: 1080})
-  await page.screenshot({path: 'output.png'});
+
+  for (let i = 0; i < shotList.length; i++) {
+    let filename = shotList[i].replace(/^.+:\/\//, '').replace(/(\/|\\)/, '-') + ".png";
+    await page.goto(shotList[i],  {waitUntil: 'networkidle2'});
+    await page.screenshot({path: filename, fullPage: true});
+  }
 
   await browser.close();
 })();
