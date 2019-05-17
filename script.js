1d1f400821c99b9ef6de4acb5cd99b1af48911df
Taylor Smith <tsmith@tsmithcreative.com>
2019-05-17 18:58:19 +0000

    Swap over the little capture script to use Puppeteer's sample code

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var page = require('webpage').create();
-page.viewportSize = {width: 1920, height: 1080};
-page.open('http://github.com/', function() {
-  window.setTimeout(function(){
-    page.render('github.png');
-    phantom.exit();
-  }, 500);
-});
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.tsmithcreative.com');
+  await page.setViewport({width: 1920, height: 1080})
+  await page.screenshot({path: 'output.png'});
+
+  await browser.close();
+})();
